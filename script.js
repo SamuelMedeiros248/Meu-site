78e45209212263fe5b3e51723154562b90043539
SamuelMedeiros248 <samuel.medeiros137.br@gmail.com>
2026-03-03 11:49:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function TC() {
   let InputTarefa = document.getElementById("inputTarefa");
   let tarefa = InputTarefa.value;
-  document.getElementById("TudoCerto").textContent = tarefa;
+  //document.getElementById("TudoCerto").textContent = tarefa;
+
+  let listaTarefa = document.getElementById("lista");
+  let novaTarefa = document.createElement("li");
+
+  novaTarefa.textContent = tarefa;
+
+  listaTarefa.appendChild(novaTarefa);
+
   InputTarefa.value = "";
 }
